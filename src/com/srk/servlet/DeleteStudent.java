b3ac89c964410b31cbd6014897ae7012fc137ad3
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2018-03-15 15:28:49 -0600

    Testing new quality gate

--- src/com/srk/servlet/DeleteStudent.java
+++ src/com/srk/servlet/DeleteStudent.java
@@ -20,6 +20,19 @@ public class DeleteStudent extends HttpServlet {
        response.sendRedirect("viewStudents");  
    }  
 	
+	//1
+	//2
+	//3
+	//4
+	//5
+	//6
+	//7
+	//8
+	//9
+	//10
+	//11
+	//12
+	
 	/*
 	1
 	2
